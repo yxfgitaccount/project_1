15a0f7b7c458c8ab2ae638d2067a399c87734f71
yxfgitaccount <115622292+yxfgitaccount@users.noreply.github.com>
2022-10-13 08:54:09 +0800

    Update cmd_test.py

--- cmd_test.py
+++ cmd_test.py
@@ -11,4 +11,4 @@ parser=argparse.ArgumentParser()
 parser.add_argument('--square',help='please enter a number:',type=int,default=100)#当给了默认值是可以不传参直接运行不报错
 #parser.add_argument('input_str',help='please enter a character:',type=str)
 args=parser.parse_args()
-print(args.square**2)
+print(args.square**3)
